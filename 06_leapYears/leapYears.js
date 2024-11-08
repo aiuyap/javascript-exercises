a9f2bc1b810a50ccfc77d8a3f4c952de70238f69
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-08 14:38:19 +0800

    Completed js-exercise/06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divBy4 = year % 4;
+    const divBy100 = year % 100;
+    const divBy400 = year % 400;
 
+    if (divBy4 === 0) {
+        if (divBy100 === 0 && divBy400 !== 0) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
