b739ed710f0ce8918b6a98dc6a6fa50afadfd722
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-08 11:48:06 +0800

    Completed js-exercise/03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    let stringCount = string.length;
+    for (; stringCount >= 0; stringCount--) {
+        reversed += string.charAt(stringCount);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
