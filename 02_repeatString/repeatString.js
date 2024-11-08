ece4cde68f3a4a81d26423e529b98277ccd29e1b
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-08 11:31:39 +0800

    Completed js-exercise/02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, mult) {
+    let word = "";
+    if (mult < 0)
+        return "ERROR"
 
+    for (let i = 0; i < mult; i++) {
+        word += str;
+    }
+    return word;
 };
 
 // Do not edit below this line
