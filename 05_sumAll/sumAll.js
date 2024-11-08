c657f2dd405cfeb4e805aab310c0fcf6a2960754
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-08 14:14:48 +0800

    Completed js-exercise/05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    let totalSum = 0;
+    let firstNum = num1;
+    let secNum = num2;
+    if ( num1 > num2 ) {
+        firstNum = num2;
+        secNum = num1;
+    }
+    for (; firstNum <= secNum; firstNum++) {
+        totalSum += firstNum;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
