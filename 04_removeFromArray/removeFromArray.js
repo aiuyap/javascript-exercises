20ce74be8285390af5cb315c410f09f531dc7bc0
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-08 13:08:57 +0800

    Completed js-exercise/04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+    for (let x = 0; x <= toBeRemoved.length; x++) {
+        const index = array.indexOf(toBeRemoved[x]);
+        if (index > -1) {
+            array.splice(index, 1);
+            x--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
