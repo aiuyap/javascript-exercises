d0bd2405d8f86c7bde08837fb1aa527babaf6f75
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-12 16:06:09 +0800

    Completed js-exercises/09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let removePunctuation = str.split("").filter((str) => {
+        if (str === "," || str === " " || str === "." || str === "!") {
+            return false;
+        }
+        return true;
+    });
 
+    const final = removePunctuation.join().toLowerCase();
+    const reverse = removePunctuation.reverse().join().toLowerCase();
+
+    if (final === reverse) {
+        return true;
+    }
+    return false;
 };
+
+console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
