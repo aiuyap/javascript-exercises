1e50cfac122f37fbbe55fafced19f5b771f609ca
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-12 15:37:52 +0800

    Completed js-exercises/08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, arr) => total += arr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, arr) => total *= arr, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let total = 1;
+  for (let x = 1; x <= power; x++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num !== 0) {
+    let total = 1;
+    for (let x = num; num >= 1; num--) {
+      total *= num;
+    }
+    return total;
+  }
+  return 1;
 	
 };
 
