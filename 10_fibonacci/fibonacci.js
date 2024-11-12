f3acc8a1554f285f8804e6e674ee2f854e81af5b
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-12 16:31:26 +0800

    Completed js-exercise/10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fib = [1, 1]
+    index = +index;
 
+    if (index === 0)
+        return 0;
+
+    for (let i = 2; i <= index; i++) {
+        fib[i] = fib[i - 2] + fib[i -1];
+    }
+
+    if (index >= 0) {
+        return fib[(index)-1];
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
