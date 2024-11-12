b2e091f376bdfabd4b152c3064b4e9cf8e905e9d
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-12 17:37:31 +0800

    Completed js-exercise/12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+    const oldestPerson = person.reduce((currentOldest, currentPerson) => {
+        let age = 0;
+        let oldestAge = 0;
+        if (currentPerson.yearOfDeath === undefined) {
+             age = new Date().getFullYear() - currentPerson.yearOfBirth;
+        } else {
+             age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+        
+        if (currentOldest.yearOfDeath === undefined) {
+            oldestAge = new Date().getFullYear() - currentOldest.yearOfBirth;
+        } else {
+            oldestAge = currentOldest.yearOfDeath - currentOldest.yearOfBirth;
+        }
 
+        if(age >= oldestAge) {
+            currentOldest = currentPerson;
+        }
+        return currentOldest;
+    });
+    return oldestPerson;
 };
 
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
